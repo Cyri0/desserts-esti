3803db8f54886efdb63dd6bea4312eb24e99ffd3
Cyri0 <you@example.com>
2025-02-11 17:38:27 +0100

    add to cart btn component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddToCartBtn from './components/AddToCartBtn'
+
+const App = () => {
+  return (
+    <div>
+      <AddToCartBtn />
+      <AddToCartBtn />
+      <AddToCartBtn />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddToCartBtn.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import styles from "./AddToCartBtn.module.css"
+
+const AddToCartBtn = () => {
+  
+  const[pieceNum, setPieceNum] = useState(0)
+  
+  const increase = () => {
+    setPieceNum(pieceNum + 1)
+  }
+  const decrease = () => {
+    setPieceNum(pieceNum - 1)
+  }
+
+  return (
+    <>
+    {
+    pieceNum == 0 ?
+        <div className={styles.addBtn} onClick={increase}>
+            <i className="fa-solid fa-cart-plus"></i>
+            Add to Cart
+        </div>
+    :    
+        <div className={styles.addBtn + " " + styles.selected}>
+            <button onClick={decrease}><i class="fa-solid fa-minus"></i></button>
+            {pieceNum}
+            <button onClick={increase}><i class="fa-solid fa-plus"></i></button>
+        </div>
+    }
+    </>
+  )
+}
+
+export default AddToCartBtn
